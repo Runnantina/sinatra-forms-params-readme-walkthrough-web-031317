7fb0f5f883170c6e77b290f00e5659363cf39d5a
Runnan Zhong <r.tina.zhong@gmail.com>
2017-04-04 17:18:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -8,5 +8,11 @@ class App < Sinatra::Base
 
   # Add your post route and action below
 
+  post '/food' do
+    # binding.pry
+    
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end
+
 
 end
